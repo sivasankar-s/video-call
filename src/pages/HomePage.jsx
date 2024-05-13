663086c51a577dfb80817f89647e50e2e1a318b6
Sivasankar <73739708+sivasankar-s@users.noreply.github.com>
2024-05-13 10:47:28 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,11 +14,13 @@ const HomePage = () => {
     {/* <h1 className='text-8xl font-semibold'>Connect with <span className='bg-gradient-to-r from-blue-700 to-blue-400 text-transparent bg-clip-text'>Alumni</span> </h1> */}
     {/* <Link to='/alumniHome' className='p-3 bg-blue-500 font-semibold text-lg'>Video Call</Link> */}
 
-    <div className='mt-24'>
-
-    <Link to='/search' className='p-3 bg-blue-500 font-semibold text-lg mr-3'>Search People</Link>
-    <Link to='/alumniHome' className='p-3 bg-blue-500 font-semibold text-lg'>Chatroom</Link>
-    <h1 className='text-4xl'>{user?.data.result.name}</h1>
+    <div className='h-[90vh] flex flex-col justify-center items-center'>
+      <h1 className='text-7xl font-semibold mb-10'>Welcome to AlumniHub</h1>
+      <h3 className='text-lg font-semibold mb-10'>A place where students and alumni can find each other, interact, collaborate and network..</h3>
+      <div className='flex justify-center'>
+        <Link to='/search' className='p-3 bg-blue-500 font-semibold text-lg mr-7 rounded-lg'>Search People</Link>
+        <Link to='/chatroom' className='p-3 border-2 border-black font-semibold text-lg rounded-lg'>Chatroom</Link>
+      </div>
     </div>
     
     </>
